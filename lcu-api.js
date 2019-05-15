c3cd8df9d0f6a2f1e160476d9da2f4ab6771ef9e
ArthurSaurel <arthur.saurel@hotmail.fr>
2019-05-15 12:14:01 +0200

    Add files via upload

---
+++ lcu-api.js
@@ -0,0 +1,83 @@
+const WebSocket = require('ws');
+const request = require('request');
+
+var ws = null;
+var conn_data = null;
+
+function bind(data, api_event) {
+	conn_data = data;
+	ws = new WebSocket(`wss://${data.username}:${data.password}@${data.address}:${data.port}/`, "wamp", {
+		rejectUnauthorized: false,
+	});
+
+	ws.on('error', (err) => {
+		//console.log(err);
+		if (err.message.includes('ECONNREFUSED')) {
+			destroy();
+			setTimeout(function () {
+				bind(data, api_event);
+			}, 1000);
+		}
+	});
+
+	ws.on('message', (msg) => {
+		var res;
+		try {
+			res = JSON.parse(msg);
+		} catch(e) {
+			console.log(e);
+        }
+		if(res[0] === 0) {
+			//console.log("connected", res);
+		}
+		if(res[1] == "OnJsonApiEvent") {
+			var evt = res[2];            
+			//console.log(`${evt.uri}:${evt.eventType}`);			
+		    if (!require('./index.js').isPaused()) api_event.emit(`${evt.uri}:${evt.eventType}`, evt.data);
+		}
+	});
+
+	ws.on('open', () => {
+		ws.send('[5, "OnJsonApiEvent"]');
+	});
+}
+
+function destroy() {
+	ws.removeEventListener()
+	ws = null;
+}
+
+var methods = {};
+["post", "put", "get", "del", "patch"].forEach(function(method) {
+	methods[method] = function(endpoint, body) {
+		return new Promise(resolve => {	
+			var options = {
+				url: `${conn_data.protocol}://${conn_data.address}:${conn_data.port}${endpoint}`,
+				auth: {
+					"user": conn_data.username,
+					"pass": conn_data.password
+				},
+				headers: {
+					'Accept': 'application/json'
+				},
+				json: true,
+				body: body,
+				rejectUnauthorized: false
+			};
+			
+			//console.log(options);
+
+			request[method](options, (error, response, data) => {
+				
+				if (error || response.statusCode != 200) {
+					resolve(response);
+					return;
+				} 
+
+				resolve(data);
+			});
+		});
+	};
+});
+
+module.exports = Object.assign({bind, destroy}, methods);
